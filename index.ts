bb4d971d639da12a0eecfb872dcced96c945ec21
richard483 <richard.william483@gmail.com>
2022-12-08 04:05:36 +0700

    added master-2

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 import { Client, Intents } from "discord.js";
 import { Bot } from "./structs/Bot";
 
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 export const bot = new Bot(
   new Client({
     restTimeOffset: 0,
